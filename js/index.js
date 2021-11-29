5741836b142482ef21a89dad7ed11d276c4b1f85
Michael Kiprotich <michael.kiprotich@student.moringaschool.com>
2021-11-29 07:08:46 +0300

    adding prices fo crust and pizza size

--- js/index.js
+++ js/index.js
@@ -16,6 +16,112 @@ $(document).ready(function(){
      $.each($("input[name='toppings']:checked"), function(){            
          pizzaTopping.push($(this).val());
      });
+     switch(pizzaSize){
+        case "0":
+          price =0;
+        break;
+        case "small":
+           price = 400;
+        //    console.log(price);
+         break;
+         case "medium":
+           price = 600;
+        //    console.log("The price is "+price);
+         break;
+         case "large":
+           price = 800;
+        //    console.log(price);
+        //  default:
+        //    console.log("error"); 
+       }
+       switch(pizzaCrust){
+          case "0":
+            crust_price = "0";
+          break;
+          case "Crispy":
+            crust_price = 250;
+          break;
+          case "Stuffed":
+            crust_price = 200;
+          break;
+          case "Gluten-free":
+            crust_price = 300;
+          break;
+        //   default:
+            // console.log("No price"); 
+        }
+        let toppingValue = pizzaTopping.length*100;
+        // console.log("toppings value" + toppingValue);
+    
+        if((pizzaSize == "0") && (pizzaCrust == "0")){
+        //   console.log("nothing selected");
+        //   $("button.proceed").show();
+        //   $("#information").show();
+        //   $("div.choice").hide();
+          alert("Please select pizza size and crust"); 
+        }
+        // else{
+        //   $("button.proceed").hide();
+        //   $("#information").hide();
+        //   $("div.choice").slideDown(1000);
+        // }
+    
+        total = price + crust_price + toppingValue;
+        // console.log(total);
+        let checkoutTotal =0;
+        // checkoutTotal = checkoutTotal + total;
+        checkoutTotal += total;
+    
+        $("#pizzaname").html($(".name option:selected").val());
+        $("#pizzasize").html( $("#size option:selected").val());
+        $("#pizzacrust").html($("#crust option:selected").val());
+        $("#pizzatopping").html(pizzaTopping.join(", "));
+        $("#totalcost").html(total);
+        
+        $("button.anotherOrder").click(function(){
+          let pizzaName = $(".name option:selected").val();
+          let pizzaSize = $("#size option:selected").val();
+          let pizzaCrust = $("#crust option:selected").val();
+          let pizzaTopping = [];
+          $.each($("input[name='toppings']:checked"), function(){            
+              pizzaTopping.push($(this).val());
+          });
+        //   console.log(ptopping.join(", "));
+          switch(pizzaSize){
+            case "0":
+              price =0;
+            break;
+            case "small":
+               price = 400;
+            //    console.log(price);
+             break;
+             case "medium":
+               price = 600;
+            //    console.log("The price is "+price);
+             break;
+             case "large":
+               price = 800;
+            //    console.log(price);
+            //  default:
+            //    console.log("error"); 
+           }
+           switch(pizzaCrust){
+              case "0":
+                crust_price = 0;
+              break;
+              case "Crispy":
+                crust_price = 250;
+              break;
+              case "Stuffed":
+                crust_price = 200;
+              break;
+              case "Gluten-free":
+                crust_price = 300;
+              break;
+            //   default:
+                // console.log("No price"); 
+            }
+            
 
 
 
